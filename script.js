c270c7d385179688adca4a28046bca7f76613f2d
Brian Cabrera <57054546+brianmateocabrera@users.noreply.github.com>
2026-08-19 13:44:47 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-    document.querySelectorAll('.category-item, .product-card, .nav-item').forEach(item => {
-      item.addEventListener('click', (e) => {
-        if(item.classList.contains('nav-item')) {
-          document.querySelectorAll('.nav-item').forEach(nav => nav.classList.remove('active'));
-          item.classList.add('active');
-        }
-      });
-    });
