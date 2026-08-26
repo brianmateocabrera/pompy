d750d2ec51f106e4c3669dafa38a25d54e1bb578
Brian Cabrera <57054546+brianmateocabrera@users.noreply.github.com>
2026-08-26 16:44:12 -0300

    Actualizar index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const API_URL = '/api/crud';
 const PATH_JSON = 'data/productos.json';
 const PATH_BANNERS = 'data/banners.json';
-const WHATSAPP_NUMERO = '549351818944';
+const WHATSAPP_NUMERO = '5493518189444';
 
 const MENSAJE_WHATSAPP =
     'Hola, quisiera consultar por este producto:';
